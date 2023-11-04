9f943b13458a2fefe19976860bad9298e6d62496
KaKashi1210 <desmondaditya@gmail.com>
2023-11-04 21:37:13 +0530

    feat: wtf is chatgpt doin but it works

--- src/pages/api/summarize.ts
+++ src/pages/api/summarize.ts
@@ -1,38 +1,32 @@
-import { Configuration, OpenAIApi } from "openai-edge";
-import { OpenAIStream, StreamingTextResponse } from "ai";
-
-const config1 = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
+import { OpenAI } from "openai";
 
 if (!process.env.OPENAI_API_KEY) {
-  throw new Error('Missing env var')
+  throw new Error("Missing env var from OpenAI");
 }
 
-const openai = new OpenAIApi(config1)
+export default async function handler(req: any, res: any) {
 
-export const config = {
-  runtime: 'edge'
-}
-
-export default async function handler(
-  req: any,
-  res: any
-) {
-
-  const { text } = req.json()
-
-  if (req.method === 'POST') {
-    const response = await openai.createCompletion({
-      model: 'gpt-3.5-turbo',
-      stream: true,
-      temperature: 0.6,
-      prompt: `Give mermaid markdown for the analogy present in text: ${text}`
-    });
-    const stream = OpenAIStream(response)
-    return new StreamingTextResponse(stream)
+  if (req.method !== 'POST') {
+    return new Response('Method not allowed', { status: 405 })
   }
-  else {
-    res.status(400).json({ error: 'Method does not exist' })
-  }
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  })
+
+  const { text } = await req.body
+
+  const stream = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: "You are a proficient AI with a specialty in explaining underlying concept into simple easy to understand analogies. Your goal here is to read the below text and explain the underlying analogies" },
+      { role: "user", content: `${text}` },
+    ],
+    max_tokens: 2000,
+    top_p: 1,
+    temperature: 0.66,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  })
+
+  res.json(stream)
 }

75419ad1011dac888c0feff1f267e4c9fbf334ce
aryan bramhane <aryanbramhane1@gmail.com>
2023-11-04 21:28:09 +0530

    feat: added shadcn, and auth completed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
+import { SessionProvider } from "next-auth/react";
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <SessionProvider>
+          <Main />
+          <NextScript />
+        </SessionProvider>
       </body>
     </Html>
-  )
+  );
 }
